1cb93b53dc96d6ade3b92f38f511424b9464ee00
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-21 21:19:02 +0530

    it is not working

--- src/DadJoke.js
+++ src/DadJoke.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 
 class DadJoke extends Component{
+    async componentDidMount(){
+        const options = {
+            method: 'GET',
+            url: 'https://dad-jokes7.p.rapidapi.com/dad-jokes/joke-of-the-day',
+            headers: {
+              'X-RapidAPI-Key': '8b7adbee44msh21d417677ec0ff0p1b190fjsnf806fcffae7a',
+              'X-RapidAPI-Host': 'dad-jokes7.p.rapidapi.com'
+            }
+          };
+        let joke = await axios.request(options,{
+            headers: "application/json"
+        });
+        console.log(joke);
+    }
     render(){
         return(
             <div>
