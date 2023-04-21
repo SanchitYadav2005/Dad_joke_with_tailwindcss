e469c803d518ea2c71f959ff0b38d2eb474c2f72
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-21 20:29:37 +0530

    tailwind is working

---
+++ src/DadJoke.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class DadJoke extends Component{
+    render(){
+        return(
+            <div>
+                <h1 className="text-zinc-800 text-xs">Hello</h1>
+            </div>
+        )
+    }
+}
+
+export default DadJoke;
